ab74a318cdf992d1932eb520dca0343c7b06b6ca
himanshuAthena <Himanshu@athenafintech.com>
2025-12-07 10:57:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
+
+MARKET_QUERIES = [
+    "equipment leasing software",
+    "lease management SaaS",
+    "asset finance software",
+    "IFRS 16 lessor accounting",
+    "rental management platform",
+    "loan and lease management system",
+    "fintech lending platform"
+]
+
+COMPETITORS = [
+    "LeaseQuery",
+    "Odessa leasing software",
+    "ALFA Systems",
+    "LTIMindtree leasing platform"
+]
